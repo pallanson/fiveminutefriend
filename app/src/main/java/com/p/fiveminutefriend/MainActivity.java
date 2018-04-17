572dd806e525b31f776697e6c3315f90362f0f0a
Philip Allanson <philip.allanson@gmail.com>
2018-04-17 15:28:21 +0200

    blank

--- app/src/main/java/com/p/fiveminutefriend/MainActivity.java
+++ app/src/main/java/com/p/fiveminutefriend/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String text = "Hello World!";
+        String text = "Hello Worldfds!";
         Log.d(text, text);
     }
 }
