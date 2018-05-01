c084bd0bec4f86fffb6b6725c96b5f5b02ed888c
Charlie <charlie@tunasson.com>
2018-05-01 18:34:23 +0200

    Firebase connections and Login functionality
    Login functionality added with minimum error
    handling

--- app/src/main/java/com/p/fiveminutefriend/LoginActivity.kt
+++ app/src/main/java/com/p/fiveminutefriend/LoginActivity.kt
@@ -3,8 +3,12 @@ package com.p.fiveminutefriend
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -21,6 +25,34 @@ class LoginActivity : AppCompatActivity() {
         loginButton = findViewById(R.id.button_sign_in_login)
         emailOrUsername = findViewById(R.id.edit_email_login)
         password = findViewById(R.id.edit_password_login)
-        
+
+        loginButton.setOnClickListener({
+            performLogin(emailOrUsername.text.toString().trim(),
+                    password.text.toString().trim())
+        })
     }
+
+    private fun performLogin(emailOrUsername: String?, password: String?) {
+        if (emailOrUsername !is String || emailOrUsername!!.isEmpty()) {
+            Toast.makeText(this,
+                    "email or username invalid",
+                    Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password !is String || password!!.isEmpty()) {
+            Toast.makeText(this,
+                    "password invalid",
+                    Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(emailOrUsername, password)
+                .addOnCompleteListener({
+
+                    Toast.makeText(this,
+                            FirebaseAuth.getInstance().currentUser!!.email.toString(),
+                            Toast.LENGTH_SHORT).show()
+                })
+    }
+
 }
