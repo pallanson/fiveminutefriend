9fcfb9c59084e4278b964e7c109b8adbf59bb167
Martin <martin.huynh0055@stud.hkr.se>
2018-05-03 18:06:39 +0200

    Fixed RegisterFragment and Removed HomeActivity
    Title explains all

--- app/src/main/java/com/p/fiveminutefriend/RegisterFragment.kt
+++ app/src/main/java/com/p/fiveminutefriend/RegisterFragment.kt
@@ -32,7 +32,7 @@ class RegisterFragment : Fragment() {
 
         button_create_account_register.setOnClickListener({
 
-            if(edit_email_register.text.isBlank()
+            if (edit_email_register.text.isBlank()
                     || edit_first_name_register.text.isBlank()
                     || edit_last_name_register.text.isBlank()
                     || edit_username_register.text.isBlank()
@@ -42,25 +42,21 @@ class RegisterFragment : Fragment() {
                         "Please fill in all fields.",
                         Toast.LENGTH_LONG)
                         .show()
-            }
-
-            else if (text_age_register.text == "Select"
+            } else if (text_age_register.text == "Select"
                     || text_gender_register.text == "Select"
                     || text_language_register.text == "Select") {
                 Toast.makeText(activity,
                         "Please select an age, gender and language.",
                         Toast.LENGTH_SHORT)
                         .show()
-            }
-
-            else {
+            } else {
                 val confirmationDialog = AlertDialog.Builder(activity)
                 confirmationDialog.setTitle("Register Account?")
                         .setMessage("You will be logged in once registering.")
                         .setPositiveButton("Register") { dialog, which ->
-                            registerUser(edit_email_register.text.toString().trim(),
-                                    edit_first_name_register.text.toString().trim(),
+                            registerUser(edit_first_name_register.text.toString().trim(),
                                     edit_last_name_register.text.toString().trim(),
+                                    edit_username_register.text.toString().trim(),
                                     edit_email_register.text.toString().trim(),
                                     edit_password_register.text.toString().trim(),
                                     text_select_age_register.text.toString().toInt(),
@@ -68,41 +64,36 @@ class RegisterFragment : Fragment() {
                                     text_language_register.text.toString().trim())
                         }
                         .setNegativeButton("Cancel") { dialog, which ->
-                            Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT)
+                            Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
                         }
                         .create()
                         .show()
             }
         })
-
         text_select_gender_register.setOnClickListener({
             createNumberPicker(0)
         })
-
         text_select_age_register.setOnClickListener({
             createNumberPicker(1)
         })
-
         text_select_language_register.setOnClickListener({
             createNumberPicker(2)
         })
-
     }
 
-    fun registerUser(firstName: String,
-                     lastName: String,
-                     username: String,
-                     email: String,
-                     password: String,
-                     age: Int,
-                     gender: String,
-                     language: String) {
+    private fun registerUser(firstName: String,
+                             lastName: String,
+                             username: String,
+                             email: String,
+                             password: String,
+                             age: Int,
+                             gender: String,
+                             language: String) {
 
         val dbReference = FirebaseDatabase.getInstance()
                 .getReferenceFromUrl(Constants.FIREBASE_USERS)
         val firebaseAuth = FirebaseAuth.getInstance()
         var genderInt = 0
-        var languageInt = 0
 
         when (gender) {
             "Male" -> genderInt = 0
@@ -115,98 +106,100 @@ class RegisterFragment : Fragment() {
                     if (task.isSuccessful) {
                         dbReference
                                 .child(FirebaseAuth.getInstance().uid)
-                                .setValue(User(firebaseAuth.uid.toString(),
-                                        firstName,
-                                        lastName,
-                                        username,
-                                        email,
-                                        language,
-                                        age,
-                                        genderInt))
-                    }
-                    else {
+                                .setValue(
+                                        User(
+                                                firebaseAuth.uid.toString(),
+                                                firstName,
+                                                lastName,
+                                                username,
+                                                email,
+                                                language,
+                                                age,
+                                                genderInt))
+                    } else {
                         Toast.makeText(activity, "Something went wrong", Toast.LENGTH_LONG)
                                 .show()
                     }
                 })
 
-        val intent = Intent(activity, HomeActivity::class.java)
+        val intent = Intent(activity, MainActivity::class.java)
         startActivity(intent)
-
     }
 
-    fun createNumberPicker(type: Int) {
-        if (type == 0) {
-            val genderValues = arrayOf("Male", "Female", "Other")
-            val genderPicker = NumberPicker(activity)
-            genderPicker.minValue = 0
-            genderPicker.maxValue = genderValues.size - 1
-            genderPicker.displayedValues = genderValues
-            genderPicker.wrapSelectorWheel = true
+    private fun createNumberPicker(type: Int) {
+        when (type) {
+            0 -> {
+                val genderValues = arrayOf("Male", "Female", "Other")
+                val genderPicker = NumberPicker(activity)
+                genderPicker.minValue = 0
+                genderPicker.maxValue = genderValues.size - 1
+                genderPicker.displayedValues = genderValues
+                genderPicker.wrapSelectorWheel = true
 
-            val dialogBuilder = AlertDialog.Builder(activity)
-            dialogBuilder.setView(genderPicker)
-                    .setTitle("Select Gender")
-                    .setPositiveButton("Ok") { dialog, which ->
-                        if (genderPicker.value == 0) {
-                            text_select_gender_register.text = "Male"
-                        } else if (genderPicker.value == 1) {
-                            text_select_gender_register.text = "Female"
-                        } else if (genderPicker.value == 2) {
-                            text_select_gender_register.text = "Other"
+                val dialogBuilder = AlertDialog.Builder(activity)
+                dialogBuilder.setView(genderPicker)
+                        .setTitle("Select Gender")
+                        .setPositiveButton("Ok") { dialog, which ->
+                            when {
+                                genderPicker.value == 0 -> text_select_gender_register.text = "Male"
+                                genderPicker.value == 1 -> text_select_gender_register.text = "Female"
+                                genderPicker.value == 2 -> text_select_gender_register.text = "Other"
+                            }
                         }
-                    }
-                    .setNegativeButton("Cancel") { dialog, which ->
-                        Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
-                    }
-                    .create()
-                    .show()
-        } else if (type == 1) {
-            var ageValues = Array<String>(100, { "$it"} )
-
-            val agePicker = NumberPicker(activity)
-            agePicker.minValue = 1
-            agePicker.maxValue = 100
-            agePicker.displayedValues = ageValues
-            agePicker.wrapSelectorWheel = true
-
-            val dialogBuilder = AlertDialog.Builder(activity)
-            dialogBuilder.setView(agePicker)
-                    .setTitle("Select Age")
-                    .setPositiveButton("Ok") { dialog, which ->
-                        text_select_age_register.text = (agePicker.value - 1).toString()
-                    }
-                    .setNegativeButton("Cancel") { dialog, which ->
-                        Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
-                    }
-                    .create()
-                    .show()
-
-        } else if (type == 2) {
-            val languageValues = arrayOf("English", "Swedish")
-            val languagePicker = NumberPicker(activity)
-
-            languagePicker.minValue = 0
-            languagePicker.maxValue = languageValues.size - 1
-            languagePicker.displayedValues = languageValues
-            languagePicker.wrapSelectorWheel = false
-
-            val dialogBuilder = AlertDialog.Builder(activity)
-            dialogBuilder.setView(languagePicker)
-                    .setTitle("Select Language")
-                    .setPositiveButton(android.R.string.ok) { dialog, which ->
-                        if (languagePicker.value == 0) {
-                            text_select_language_register.text = "English"
-                        } else if (languagePicker.value == 1) {
-                            text_select_language_register.text = "Swedish"
+                        .setNegativeButton("Cancel") { dialog, which ->
+                            Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
                         }
-                    }
-                    .setNegativeButton(android.R.string.cancel) { dialog, which ->
-                        Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT)
-                    }
-                    .create()
-                    .show()
-        } else return
+                        .create()
+                        .show()
+            }
+            1 -> {
+                val ageValues = Array<String>(100, { "$it" })
+
+                val agePicker = NumberPicker(activity)
+                agePicker.minValue = 1
+                agePicker.maxValue = 100
+                agePicker.displayedValues = ageValues
+                agePicker.wrapSelectorWheel = true
+
+                val dialogBuilder = AlertDialog.Builder(activity)
+                dialogBuilder.setView(agePicker)
+                        .setTitle("Select Age")
+                        .setPositiveButton("Ok") { dialog, which ->
+                            text_select_age_register.text = (agePicker.value - 1).toString()
+                        }
+                        .setNegativeButton("Cancel") { dialog, which ->
+                            Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
+                        }
+                        .create()
+                        .show()
+
+            }
+            2 -> {
+                val languageValues = arrayOf("English", "Swedish")
+                val languagePicker = NumberPicker(activity)
+
+                languagePicker.minValue = 0
+                languagePicker.maxValue = languageValues.size - 1
+                languagePicker.displayedValues = languageValues
+                languagePicker.wrapSelectorWheel = false
+
+                val dialogBuilder = AlertDialog.Builder(activity)
+                dialogBuilder.setView(languagePicker)
+                        .setTitle("Select Language")
+                        .setPositiveButton(android.R.string.ok) { dialog, which ->
+                            if (languagePicker.value == 0) {
+                                text_select_language_register.text = "English"
+                            } else if (languagePicker.value == 1) {
+                                text_select_language_register.text = "Swedish"
+                            }
+                        }
+                        .setNegativeButton(android.R.string.cancel) { dialog, which ->
+                            Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show()
+                        }
+                        .create()
+                        .show()
+            }
+            else -> return
+        }
     }
-
 }
